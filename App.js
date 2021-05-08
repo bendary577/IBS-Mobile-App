93b9ac9cee0e9ef1667d8ff7551174a286b7b038
Youssef Rashad <40126657+YoussefRashad@users.noreply.github.com>
2021-05-08 21:54:35 +0200

    commit2

--- App.js
+++ App.js
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import {createAppContainer } from "react-navigation";
-import {AppNavigator} from './app/config/router';
-
-
-const AppContainer = createAppContainer(AppNavigator);
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <AppContainer />
-      <Text>About Us!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-=======
 import 'react-native-gesture-handler';
 import React from 'react';
 import StackNavigation from './app/config/router';
@@ -39,5 +10,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
->>>>>>> 64394aa3d82c56e4ca07db21977018e6ddad8249
--- app/config/router.js
+++ app/config/router.js
@@ -1,20 +1,7 @@
 
 import {createStackNavigator} from 'react-navigation-stack';
-<<<<<<< HEAD
 import AboutUs from '../screens/contacts/aboutUs';
 import VisitUs from '../screens/contacts/VisitUs';
-
-export let AppNavigator = createStackNavigator({
-    AboutUs : {
-      screen: AboutUs,
-      visitUs: VisitUs
-    }
-  },{
-    initialRouteName: "AboutUs"
-});
-
-=======
-import AboutUs from '../screens/contacts/AboutUs';
 
 const Stack = createStackNavigator();
 
@@ -22,7 +9,8 @@ export const StackNavigation = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen name="AboutUs" component={AboutUs} />
+      <Stack.Screen name="VisitUs" component={VisitUs} />
     </Stack.Navigator>
   );
 }
->>>>>>> 64394aa3d82c56e4ca07db21977018e6ddad8249
+
